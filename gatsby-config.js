beacea2c7577a77ef009242f5ea8457b552bcbe0
hvanyo <hvanyo@its.jnj.com>
2020-12-04 17:52:50 -0600

    update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,6 @@
 const express = require('express');
 
-const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
-  path: `.env.${activeEnv}`,
-});
-
-const SITEURL = process.env.SITE_URL;
 
 // Gatsby plugins list.
 const plugins = [
@@ -27,7 +21,7 @@ const plugins = [
   {
     resolve: 'gatsby-plugin-canonical-urls',
     options: {
-      siteUrl: SITEURL,
+      siteUrl: 'http://solutionsbyheidi.com/',
     },
   },
   {
@@ -35,33 +29,12 @@ const plugins = [
   },
 ];
 
-const tagManagerEnabled = (process.env.GOOGLE_TAGMANAGER_ENABLED || '1') === '1';
-if (tagManagerEnabled) {
-  /**
-   * Google Tag Manager plugin.
-   */
-  plugins.push({
-    resolve: 'gatsby-plugin-google-tagmanager',
-    options: {
-      id: process.env.GOOGLE_TAGMANAGER_ID || 'GTM-XXXXXXX',
-      // datalayer to be set before GTM is loaded
-      // should be an object or a function that is executed in the browser
-      // Defaults to null
-
-      // Specify optional GTM environment details.
-      // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-      // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-      dataLayerName: 'globalDataLayer',
-    },
-  });
-}
-
 module.exports = {
   developMiddleware: app => {
     app.use('/___docs', express.static('doc'));
   },
   siteMetadata: {
-    siteUrl: SITEURL,
+    siteUrl: 'http://solutionsbyheidi.com/',,
   },
   plugins,
 };
